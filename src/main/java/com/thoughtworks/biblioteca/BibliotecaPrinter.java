72bc7d968f55b018ea83cdd8f5a166ae01be3c88
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-06-11 13:49:33 -0300

    Create a function printAllBooks

--- src/main/java/com/thoughtworks/biblioteca/BibliotecaPrinter.java
+++ src/main/java/com/thoughtworks/biblioteca/BibliotecaPrinter.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.biblioteca;
 
+import java.util.List;
+
 public class BibliotecaPrinter {
 
     public void print(String message) {
         System.out.println(message);
+    }
+
+    public void printAllBooks(List<String> listOfBooks) {
+
+        String books = "";
+
+
+        for (int i = 0; i < listOfBooks.size(); i++) {
+            books += listOfBooks.get(i) + "\n";
+        }
+
+        System.out.println(books);
+
     }
 }
