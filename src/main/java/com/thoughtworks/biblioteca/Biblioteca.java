0adfe7dc813da52fd76d46b30cb5e6196ee78986
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-06-11 16:38:33 -0300

    Refaction on function populateLibrary - Create objects to each book

--- src/main/java/com/thoughtworks/biblioteca/Biblioteca.java
+++ src/main/java/com/thoughtworks/biblioteca/Biblioteca.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.biblioteca;
 
+import com.thoughtworks.biblioteca.Models.Book;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,7 @@ public class Biblioteca {
 
     BibliotecaPrinter printer;
 
-    List<String> booksOnLibrary = new ArrayList<String>();
+    List<Book> booksOnLibrary = new ArrayList<Book>();
 
 
     public Biblioteca(BibliotecaPrinter printer) {
@@ -27,9 +29,16 @@ public class Biblioteca {
     }
 
     public void populateLibrary(){
-        booksOnLibrary.add("Java");
-        booksOnLibrary.add("PHP");
-        booksOnLibrary.add("JS");
-        booksOnLibrary.add("Python");
+
+        Book book1 = new Book("Livro de Java", "Autor do Livro de Java", 1991);
+        Book book2 = new Book("Livro de PHP", "Autor do Livro de PHP", 1993);
+        Book book3 = new Book("Livro de JS", "Autor do Livro de JS", 1995);
+        Book book4 = new Book("Livro de Python", "Autor do Livro de Python", 1992);
+
+
+        booksOnLibrary.add(book1);
+        booksOnLibrary.add(book2);
+        booksOnLibrary.add(book3);
+        booksOnLibrary.add(book4);
     }
 }
