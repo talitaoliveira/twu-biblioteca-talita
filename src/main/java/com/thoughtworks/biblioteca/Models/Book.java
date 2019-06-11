8bb197700d517d6595315c4058aa9954ad4ae978
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-06-11 13:58:32 -0300

    Folder to include some models

---
+++ src/main/java/com/thoughtworks/biblioteca/Models/Book.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.biblioteca.Models;
+
+public class Book {
+
+    private String author;
+    private String title;
+    private Integer year;
+
+    public Book(String title, String author, Integer year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getBookName() {
+        return this.title;
+    }
+
+    public String getAuthorName() {
+        return this.author;
+    }
+
+    public Integer getYear(){
+        return this.year;
+    }
+}
---
+++ src/test/java/com/thoughtworks/biblioteca/Models/BookTest.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.biblioteca.Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+    @Test
+    public void shouldValidadeBookName() {
+        Book book = new Book("AnyName Book", null, null);
+        assertEquals("AnyName Book", book.getBookName());
+    }
+
+    @Test
+    public void shouldValidateAlwaysRightName() {
+        Book book = new Book("AnyName Book 2", null, null);
+        assertEquals("AnyName Book 2", book.getBookName());
+    }
+
+    @Test
+    public void shouldValidateAuthorName() {
+        Book book = new Book("AnyNameBook", "Author Name", null);
+        assertEquals("Author Name", book.getAuthorName());
+    }
+
+    @Test
+    public void shouldValidadeYear() {
+        Integer year = 2019;
+        Book book = new Book("AnyNameBook", "Author Name", year);
+        assertEquals(year, book.getYear());
+    }
+
+}
