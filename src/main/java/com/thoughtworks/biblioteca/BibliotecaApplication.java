2c4cb5fd0f4de5b298e87a985c3be78de3b2ac5c
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-06-12 18:39:33 -0300

    Add options to menu and send menu to Biblioteca by constructor

--- src/main/java/com/thoughtworks/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/thoughtworks/biblioteca/BibliotecaApplication.java
@@ -3,7 +3,10 @@ package com.thoughtworks.biblioteca;
 public class BibliotecaApplication {
     public static void main(String[] args) {
         BibliotecaPrinter printer = new BibliotecaPrinter();
-        Biblioteca library = new Biblioteca(printer);
+        Menu menu = new Menu(printer);
+        menu.addOption(new MenuOption("list", ()-> System.out.println("Lista livros")));
+        menu.addOption(new MenuOption("search", ()-> System.out.println("Busca livros")));
+        Biblioteca library = new Biblioteca(printer, menu);
         library.start();
     }
 }
